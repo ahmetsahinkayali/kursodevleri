ed04a500447b5a0b19327c2c54b48d1d8f97498a
Ahmet Şahin Kayalı <90548561+ahmetsahinkayali@users.noreply.github.com>
2021-09-26 14:11:14 +0300

    Add files via upload

---
+++ Sorgular Ders1.sql
@@ -0,0 +1,14 @@
+﻿use Northwind
+select * from Employees
+select FirstName, LastName, Title, Birthdate from Employees
+select * from Employees
+select [EmployeeID], [LastName], [FirstName], [Title], [TitleOfCourtesy], [BirthDate], [HireDate], [Address], [City], [Region], [PostalCode], [Country], [HomePhone], [Extension], [Photo], [Notes], [ReportsTo], [PhotoPath] from Employees
+select FirstName as Ad, LastName as Soyad, Title as Görev from Employees
+select FirstName Ad, LastName Soyad, Title Görev, BirthDate DoğumTarihi, BirthDate Doğum_Tarihi, BirthDate 'Doğum Tarihi' from Employees
+select City from Employees 
+select DISTINCT City FROM Employees
+SELECT DISTINCT City, FirstName from Employees
+select TitleOfCourtesy, Firstname, LastName from Employees
+select TitleOfCourtesy +FirstName+ LastName as Personel from Employees
+
+select Employees = CONCAT(TitleOfCourtesy, '', FirstName, '', LastName) from Employees
---
+++ Sorgular Ders2.sql
@@ -0,0 +1,59 @@
+﻿use Northwind;
+
+select EmployeeID, TitleOfCourtesy as Ünvan, Ad = Firstname, LastName 'Soyadı' from Employees
+where TitleOfCourtesy = 'Mr.'
+
+select * from Employees
+
+select EmployeeID, FirstName, LastName
+from Employees where EmployeeID > 5
+
+select FirstName, LastName, YEAR(BirthDate) as[Year]from Employees where YEAR(BirthDate) = 1960
+
+select EmployeeID, FirstName, LastName, BirthDate from Employees
+
+ where YEAR(BirthDate) >= 1950 and YEAR(BirthDate) <= 1961
+
+select FirstName, LastName, BirthDate from Employees
+
+where YEAR(BirthDate) between 1950 and 1961
+
+select FirstName, LastName,Title, TitleOfCourtesy, Country, BirthDate from Employees
+
+where Country = 'UK' and
+
+( TitleOfCourtesy = 'Ms.' or TitleOfCourtesy = 'Mrs.')
+
+select TitleOfCourtesy, firstname, lastname, birthdate from Employees
+
+where TitleOfCourtesy = 'Mr.' and YEAR(BirthDate) <= 1961
+
+select firstname,lastname,titleofcourtesy,YEAR(GETDATE()) - YEAR(BirthDate) as Age from Employees Where Titleofcourtesy = 'Mr.' or YEAR(GETDATE()) - YEAR(BirthDate) >= 60
+
+select * from Employees where region is NULL
+select * from Employees where region is NOT NULL
+
+select Firstname,lastname,birthdate from Employees
+where EmployeeID between 2 and 8 
+order by firstname ASC 
+
+select Firstname,lastname,hiredate from Employees
+order by HireDate Desc 
+
+
+select FirstName, LastName from Employees
+order by FirstName, LastName desc
+
+select firstname,lastname,title,hiredate,titleofcourtesy,birthdate from Employees
+order by 1,6 desc
+
+select firstname,lastname,title,TitleOfCourtesy,birthdate from Employees
+order by TitleOfCourtesy ,YEAR(BirthDate) desc
+
+select firstname, lastname , year(birthdate) as [year] from Employees
+where year (birthdate) >= 1952 and year(birthdate) <= 1960
+order by 3
+
+select firstname, lastname, year(birthdate) as [year] from Employees
+where year(birthdate) between 1952 and 1960
+order by 3
